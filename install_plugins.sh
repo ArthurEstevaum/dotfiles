42804b913fd80017d28f58ef142434ddfa1c3dd5
ArthurEstevaum <arthurestevao631@gmail.com>
2026-01-05 13:25:15 -0300

    Add themes to install script

--- install_plugins.sh
+++ install_plugins.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 
+# Define o caminho da pasta custom (padrão ou personalizada)
 ZSH_CUSTOM=${ZSH_CUSTOM:-~/.oh-my-zsh/custom}
 
-echo "Instalando plugins do Zsh..."
-
-# Exemplo: zsh-autosuggestions
+echo "--- Instalando Plugins ---"
+# (Seus comandos de plugins que vimos antes...)
 if [ ! -d "$ZSH_CUSTOM/plugins/zsh-autosuggestions" ]; then
   git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
 fi
 
-# Exemplo: zsh-syntax-highlighting
-if [ ! -d "$ZSH_CUSTOM/plugins/zsh-syntax-highlighting" ]; then
-  git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+echo "--- Instalando Temas ---"
+
+# Powerlevel10k
+if [ ! -d "$ZSH_CUSTOM/themes/powerlevel10k" ]; then
+  echo "Baixando Powerlevel10k..."
+  git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
 fi
 
-echo "Plugins instalados!"
+echo "Tudo pronto! Reinicie o terminal."
